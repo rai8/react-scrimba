69a0ea66cd228edeadf9b8ae92cbfeb29dc49ba1
rai8 <hytonne@outlook.com>
2019-08-20 14:24:27 +0300

    property modification

--- src/App.js
+++ src/App.js
@@ -6,32 +6,40 @@ function App() {
   return (
     <div className="contacts">
       <ContactCard
-        name="Mr. Whiskerson"
-        imgUrl="http://placekitten.com/300/200"
-        alt=""
-        phone="(212) 555-1234"
-        email="mr.whiskaz@catnap.meow"
+        contact={{
+          name: "Mr. Whiskerson",
+          imgUrl: "http://placekitten.com/300/200",
+          alt: "",
+          phone: "(212) 555-1234",
+          email: "mr.whiskaz@catnap.meow"
+        }}
       />
       <ContactCard
-        name="Fluffykins"
-        imgUrl="http://placekitten.com/400/200"
-        alt=""
-        phone="(212) 555-2345"
-        email="fluff@me.com"
+        contact={{
+          name: "Fluffykins",
+          imgUrl: "http://placekitten.com/400/200",
+          alt: "",
+          phone: "(212) 555-2345",
+          email: "fluff@me.com"
+        }}
       />
       <ContactCard
-        name="Destroyer"
-        imgUrl="http://placekitten.com/400/300"
-        alt=""
-        phone="(212) 555-3456"
-        email="ofworlds@yahoo.com"
+        contact={{
+          name: "Destroyer",
+          imgUrl: "http://placekitten.com/400/300",
+          alt: "",
+          phone: "(212) 555-3456",
+          email: "ofworlds@yahoo.com"
+        }}
       />
       <ContactCard
-        name="Felix"
-        imgUrl="http://placekitten.com/200/100"
-        alt=""
-        phone="(212) 555-4567"
-        email="mr.whiskaz@catnap.meow"
+        contact={{
+          name: "Felix",
+          imgUrl: "http://placekitten.com/200/100",
+          alt: "",
+          phone: "(212) 555-4567",
+          email: "mr.whiskaz@catnap.meow"
+        }}
       />
     </div>
   );
