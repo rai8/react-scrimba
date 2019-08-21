8ba3ffaadac686eecf5815a7ef70c73a7a22e727
rai8 <hytonne@outlook.com>
2019-08-22 00:24:53 +0300

    setting up the state change

--- src/App.js
+++ src/App.js
@@ -10,10 +10,23 @@ export default class App extends Component {
     this.state = {
       todos: todosData
     };
+    this.handleChange=this.handleChange.bind(this)
   }
+   handleChange(id){
+     this.setState(prevState=>{
+       const updatedTodos=prevState.todos.map(todo=>{
+         if (todo.id===id){
+           todo.completed=!todo.completed
+         }
+         return todo
+       } )
+       return updatedTodos
+     })
+
+   }
   render() {
     const todoItems = this.state.todos.map(item => (
-      <TodoItem key={item.id} item={item} />
+      <TodoItem key={item.id} item={item} handleChange={this.handleChange}/>
     ));
     return <div className="todo-list">{todoItems}</div>;
   }
